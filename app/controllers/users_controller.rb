4023da532de436c683365adafb075afde1fba9e5
Hua Viet Ngoc <ngockingspeed@gmail.com>
2015-04-07 11:02:00 +0700

    User model + session/cookie

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,66 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new user_params_creation
+    if @user.save
+      log_in @user
+      flash[:success] = "Your account is created successfully"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes user_params_updating
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params_creation
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def user_params_updating
+    params.require(:user).permit(:email, :name, :avatar, :password, :password_confirmation)
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user?(User.find params[:id])
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,54 @@ class User < ActiveRecord::Base
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :lessons, dependent: :destroy
 
+  attr_accessor :remember_token
+  USERNAME_VALID_REGEX = /\A[A-Za-z0-9\-_]+\z/
+  validates :username,  presence: true, length: {maximum: 20},
+                        format: {with: USERNAME_VALID_REGEX},
+                        uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX}
   has_secure_password
+  validates :password, length: {minimum: 5}, allow_blank: true
+
+  mount_uploader :avatar, AvatarUploader
+  validate  :avatar_size
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+ 
+  def authenticated?(attribute, token)
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def fullname
+    self.name.blank? ? self.username : self.name
+  end
+
+  private
+
+  def avatar_size
+    if avatar.size > 2.megabytes
+      errors.add(:avatar, "should be less than 2MB")
+    end
+  end
 end
