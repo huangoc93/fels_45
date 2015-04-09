7b434fc5b16e7dfb20bf445f0c91a20763f7ac1a
Hua Viet Ngoc <ngockingspeed@gmail.com>
2015-04-09 12:45:21 +0700

    Refactor code - (seperate admin site)
    Conflicts:
    	app/assets/javascripts/application.js
    	app/assets/stylesheets/custom.scss
    	app/controllers/users_controller.rb
    	config/routes.rb
    	db/seeds.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find params[:id]
@@ -38,12 +37,6 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-  end
-
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_path
   end
 
   private
